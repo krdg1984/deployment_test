ef356944c49daca4e4af2695119a23eaa7aa21e1
Kevin Griffith <kevingriffith84@gmail.com>
2019-05-12 22:01:31 -0700

    render

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ var PORT = process.env.PORT|| 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Star Wars Characters (DATA)
 // =============================================================
 var characters = [
